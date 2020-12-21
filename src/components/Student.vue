b0eb2f6dd0464b4d909505fe43e15ea19e095c31
Manoj <manojthayil46>
2020-12-21 20:30:09 +0530

    commit 1

---
+++ src/components/Student.vue
@@ -0,0 +1,66 @@
+<style src = "./style.css"></style>
+
+<template src = "./Student.html"></template>
+
+<script>
+
+
+ export default{
+  data: function(){
+    return{
+    currentUser  : {},
+    selectedUser : null,
+    errorMsg : false,
+    successMsg : false,
+    showModal : false,
+    showDeleteModal : false,
+    showEditModal : false,
+    showPayDialog : false,
+    currentData : [{firstName : '',lastName:'',phone:'',email:'',address:''}],
+    studentData : [{firstName : 'Manoj',lastName:'Thayil',phone : '8286563959',email: 'manojthayil786@gmail.com',address: 'Mumbai'},
+                   {firstName : 'Amardeep',lastName:'Singh',phone : '7883787467',email: 'amar@986@gmail.com',address: 'Punjab'}
+    
+    ]
+   }
+ },
+ methods:{
+   addStudentData : function(){
+     this.studentData.push(this.currentData);
+     this.currentData = [];
+     this.showModal = false;
+     this.successMsg = true;
+     setTimeout(()=>{
+       this.successMsg = false;
+
+     },1000)
+   },
+
+   deleteItem : function(index){
+     console.log(index);
+     this.selectedUser = index;
+     this.showDeleteModal = true;
+   },
+   confirmDelete : function(){
+     this.studentData.splice(this.selectedUser,1);
+     this.showDeleteModal = !this.showDeleteModal;
+   },
+   updateStudentData : function(){
+     this.studentData[this.index] = this.currentData;
+   },
+   editItem : function(index,user){
+     console.log(index,user);
+     this.showEditModal = true;
+     this.index = index;
+     this.currentUser = user;
+   },
+   payFees : function(firstName){
+     this.showPayDialog = !this.showPayDialog;
+     this.firstName = firstName;
+   }
+
+ }
+ }
+
+
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Student from './components/Student';
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+const routes = [
+  { path : '/',component :Student}
+ 
+]
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router : router,
   render: h => h(App),
 }).$mount('#app')
